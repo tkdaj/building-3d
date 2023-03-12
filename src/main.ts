1084501691b77ee015f4d691c517ab82448476b0
AJ Cloutier <ajcloutier@gmail.com>
2023-03-11 23:43:51 -0600

    Add base styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import './main.css';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
