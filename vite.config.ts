17fc55294c8c9139d4d5c1d73aa06b5c16770d26
AJ Cloutier <ajcloutier@gmail.com>
2023-03-11 23:39:52 -0600

    fixed index.html path generation

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  base: '/building-3d/',
+  server: {
+    port: 3000,
+    open: true,
+    hmr: false,
+  },
 });
