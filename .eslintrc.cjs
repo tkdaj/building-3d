9de0fb286ac8c36a0c0b9a245b84e4cb6a406d06
AJ Cloutier <ajcloutier@gmail.com>
2023-03-11 22:25:10 -0600

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,78 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'implicit-dependencies', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  rules: {
+    'no-debugger': 1,
+    'no-console': 1,
+    'no-unused-vars': 0,
+    'no-restricted-globals': 0,
+    'no-unused-expressions': 0,
+    'no-unneeded-ternary': 1,
+    'no-nested-ternary': 0,
+    'no-case-declarations': 1,
+    'import/no-anonymous-default-export': 0,
+    'import/no-named-as-default': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-argument': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/unbound-method': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-unnecessary-condition': 1,
+    '@typescript-eslint/consistent-type-imports': 1,
+    '@typescript-eslint/no-confusing-non-null-assertion': 0,
+    '@typescript-eslint/no-unused-vars': [
+      1,
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'import/order': [
+      1,
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        warnOnUnassignedImports: true,
+        groups: [['builtin', 'external'], 'internal', 'index', ['parent', 'sibling']],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  root: true,
+};
